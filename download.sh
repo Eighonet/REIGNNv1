791a615739288075b53d3bcf02e9ff24d31eb87f
Porvatov Vadim <eighonet@gmail.com>
2022-05-09 01:23:00 +0300

    May update

--- download.sh
+++ download.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-mkdir processed_data
-cd processed_data
+mkdir general_data
+cd general_data
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_authors_edge_list.csv
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_authors_edges_papers_indices.csv
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_authors_features.csv
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_papers_features_vectorized_compressed_32.csv
-cd ../datasets/SSORC_CS_10_21_1437_3164_unfiltered
+cd ../datasets/CS1021small
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_10_21_1437_3164_unfiltered_authors_edges_papers_indices.csv
+cd ../CS1021medium
+wget https://sc.link/53yR
 
