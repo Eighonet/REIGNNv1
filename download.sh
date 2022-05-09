79fa2b824c128278d023cc13554e70fd06b31736
Porvatov Vadim <eighonet@gmail.com>
2022-05-09 17:06:26 +0300

    Rebuild 1

--- download.sh
+++ download.sh
@@ -6,7 +6,7 @@ wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_authors_features.csv
 wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_2010_2021_papers_features_vectorized_compressed_32.csv
 cd ../datasets/CS1021small
-wget https://n-usr-rhikf.s3pd02.sbercloud.ru/b-usr-rhikf-6g8/SSORC_CS_10_21_1437_3164_unfiltered_authors_edges_papers_indices.csv
+wget https://sc.link/mGwp
 cd ../CS1021medium
-wget https://sc.link/53yR
+wget https://sc.link/n8Z4
 
